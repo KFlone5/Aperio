95653859d76b91e3d199565950aaadec8fd1468b
Kirill <129993768+KFlone5@users.noreply.github.com>
2026-04-07 08:08:17 +0400

    Added -p argument
    -p --pattern is on-the-fly password generation based on the password pattern based on the Krunch tool.
    Added a check so that if the user specified both -w and -p arguments, the code would display a message asking to select one. The same applies if the user didn't specify both arguments.

--- Aperio.py
+++ Aperio.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 from cracker import crack_hash, HASHLIB_ALGORITHMS, CRYPT_ALGORITHMS
 
 ALL_ALGORITHMS = HASHLIB_ALGORITHMS | CRYPT_ALGORITHMS
@@ -6,31 +7,56 @@ ALL_ALGORITHMS = HASHLIB_ALGORITHMS | CRYPT_ALGORITHMS
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        prog="aperio",
+        prog = "aperio",
         description="Hash cracker using wordlist-based bruteforce"
     )
 
     parser.add_argument(
         "-H", "--hash",
-        required=True,
-        help="Target hash to crack"
+        required = True,
+        help = "Target hash to crack"
     )
 
     parser.add_argument(
         "-a", "--algorithm",
-        required=True,
-        choices=ALL_ALGORITHMS,
-        metavar="ALGORITHM",
-        help=f"Hashing algorithm to use. Available: {', '.join(sorted(ALL_ALGORITHMS))}"
+        required = True,
+        choices = ALL_ALGORITHMS,
+        metavar = "ALGORITHM",
+        help = f"Hashing algorithm to use. Available: {', '.join(sorted(ALL_ALGORITHMS))}"
     )
 
     parser.add_argument(
         "-w", "--wordlist",
-        required=True,
-        help="Path to wordlist file"
+        required = False,
+        help = "Path to wordlist file"
     )
 
-    return parser.parse_args()
+    parser.add_argument(
+        "-p", "--pattern",
+        required = False,
+        help = "Pattern for a password. (Krunch on the fly feature)"
+    )
+
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+    
+    args = parser.parse_args()
+ 
+    if args.wordlist and args.pattern:
+        print("Error: use either -w or -p, not both.\n")
+        print(f"  -w / --wordlist   {parser._option_string_actions['--wordlist'].help}")
+        print(f"  -p / --pattern    {parser._option_string_actions['--pattern'].help}")
+        print()
+        parser.print_help()
+        sys.exit(1)
+ 
+    if (not args.wordlist) and (not args.pattern):
+        print("Error: you must specify either -w (wordlist) or -p (pattern).\n")
+        parser.print_help()
+        sys.exit(1)
+
+    return args()
 
 
 def main():

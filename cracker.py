b1414d855ed35b70bf83fbca1e61f0d903583953
Kirill <129993768+KFlone5@users.noreply.github.com>
2026-04-08 21:11:00 +0400

    Added multi threading for crack_hash()

--- cracker.py
+++ cracker.py
@@ -1,5 +1,6 @@
-from algorithms import hashlib_algos, crypt_algos
+import multiprocessing
 from itertools import product
+from algorithms import hashlib_algos, crypt_algos
 
 HASHLIB_ALGORITHMS = {
     "md5", "sha1", "sha224", "sha256", "sha512",
@@ -18,6 +19,16 @@ NUM_CHARSET = "0123456789"
 SYM_CHARSET = "!@#$%^&*()-_+=~`[]{}|\\:;\"'<>,.?/"
 ALL_CHARSET = (LOW_CHARSET + UPP_CHARSET + NUM_CHARSET + SYM_CHARSET)
 
+
+def verify_password(task):
+    password, target_hash, algorithm = task
+    if algorithm in HASHLIB_ALGORITHMS:
+        if hashlib_algos.verify(password, target_hash, algorithm):
+            return password
+    elif algorithm in CRYPT_ALGORITHMS:
+        if crypt_algos.verify(password, target_hash, algorithm):
+            return password
+    return None
 
 def krunch_init(pattern, strings=""):
     total_passwords = 1
@@ -48,33 +59,40 @@ def krunch_init(pattern, strings=""):
         else:
             charsets.append(char)
 
-    print(f"Total number of passwords will be generated: {total_passwords}")
+    print(f"Total number of passwords will be checked: {total_passwords}")
     
     return charsets
 
-def verify_password(password, target_hash, algorithm):
-    if algorithm in HASHLIB_ALGORITHMS:
-        return hashlib_algos.verify(password, target_hash, algorithm)
-    elif algorithm in CRYPT_ALGORITHMS:
-        return crypt_algos.verify(password, target_hash, algorithm)
-    else:
-        raise ValueError(f"Unsupported algorithm: {algorithm}")
-
-
-def crack_hash(target_hash, wordlist_path, algorithm):
+def crack_hash(target_hash, wordlist_path, algorithm, threads=1):
     try:
-        with open(wordlist_path, "r", encoding="utf-8", errors="ignore") as f:
-            for line in f:
-                password = line.strip()
-                if not password:
-                    continue
-                print(f"Trying: {password:<30}", end="\r")
-                if verify_password(password, target_hash, algorithm):
-                    print(" " * 50, end="\r")
-                    return password
+        with open(wordlist_path, "r", encoding="utf-8", errors="ignore") as file:
+            if threads <= 1:
+                # Single thread
+                for line in file:
+                    password = line.strip()
+                    if not password:
+                        continue
+                    print(f"Trying: {password:<30}", end="\r")
+                    if verify_password((password, target_hash, algorithm)):
+                        print(" " * 50, end="\r")
+                        return password
+            else:
+                # Multi Thread with creation of task
+                tasks = []
+                for line in file:
+                    password = line.strip()
+                    if password:
+                        # Collecting data into a single task
+                        new_task = (password, target_hash, algorithm)
+                        tasks.append(new_task)
+                
+                with multiprocessing.Pool(processes=threads) as pool:
+                    for result in pool.imap_unordered(verify_password, tasks, chunksize=1000):
+                        if result:
+                            pool.terminate()
+                            return result
     except FileNotFoundError:
         print(f"Wordlist file not found: {wordlist_path}")
-        return None
     return None
 
 def crack_hash_pattern(target_hash, pattern, algorithm):
